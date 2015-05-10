e341fea3650720a512bff34315a5dd01472ea38e
MathiasKrill <mathias.krill@live.de>
2015-05-10 22:49:26 +0200

    push

--- WaiWeb/src/Dao/Interface/ImageItemInterface.java
+++ WaiWeb/src/Dao/Interface/ImageItemInterface.java
@@ -1,5 +1,8 @@
 package Dao.Interface;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.HashMap;
 import java.util.List;
 
 import model.Cam;
@@ -9,11 +12,16 @@ public interface ImageItemInterface {
 	
 
 	
-	public void addImage(ImageItem imageItem);
-	public ImageItem getImage(int imageId);
-	public void deleteImageItemFromDatabase(int imageId);
-	public void deleteImageItemFromDatabase(ImageItem imageItem);
+	public void addImage(BufferedImage bufferedimage, String );
+	public void addImage(File file);
+	
+	//Das Argument ist eine Hashmap mit den Angaben Year, Month, Day, Hour, Minute
+	public ImageItem getImageItem(HashMap<String,String> imageHashMap);
+	public void deleteImage(HashMap<String,String> imageHashMap);
+	
+	public void deleteImage(ImageItem imageItem);
 	public List<ImageItem> getAllImageItems();
+	
 	
 
 }
--- WaiWeb/src/model/ImageItem.java
+++ WaiWeb/src/model/ImageItem.java
@@ -1,5 +1,80 @@
 package model;
 
+import java.util.HashMap;
+
 public class ImageItem {
+	
+	//private long Id_Image;  // id ist nicht 
+	private long Id_CamSource;
+	private String year;
+	private String month;
+	private String week;
+	private String day;
+	private String hour;
+	private String minute;
+	private String second;
+	
+	private String Kommentar;
+	
+	public ImageItem(){
+	};
+
+	public ImageItem(long id_Image, long id_CamSource, String year,
+			String month, String week, String day, String hour, String minute,
+			String second, String kommentar) {
+		Id_Image = id_Image;
+		Id_CamSource = id_CamSource;
+		this.year = year;
+		this.month = month;
+		this.week = week;
+		this.day = day;
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+		Kommentar = kommentar;
+	}
+
+	public ImageItem(long id_CamSource, String year, String month, String week,
+			String day, String hour, String minute, String second,
+			String kommentar) {
+		super();
+		Id_CamSource = id_CamSource;
+		this.year = year;
+		this.month = month;
+		this.week = week;
+		this.day = day;
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+		Kommentar = kommentar;
+	}
+	
+	public ImageItem(long id_CamSource, HashMap<String,String> timeHashMap, String kommentar) {
+		Id_CamSource = id_CamSource;
+		this.year = timeHashMap.get("year");
+		this.month = timeHashMap.get("month");
+		this.week = timeHashMap.get("week");
+		this.day = timeHashMap.get("day");
+		this.hour = timeHashMap.get("hour");
+		this.minute = timeHashMap.get("minute");
+		this.second = timeHashMap.get("second");
+		Kommentar = kommentar;
+	}
+	
+	public ImageItem(long id_Image, long id_CamSource, HashMap<String,String> timeHashMap, String kommentar) {
+		this.Id_Image = id_Image;
+		this.Id_CamSource = id_CamSource;
+		this.year = timeHashMap.get("year");
+		this.month = timeHashMap.get("month");
+		this.week = timeHashMap.get("week");
+		this.day = timeHashMap.get("day");
+		this.hour = timeHashMap.get("hour");
+		this.minute = timeHashMap.get("minute");
+		this.second = timeHashMap.get("second");
+		Kommentar = kommentar;
+	}
+	
+	
+	
 
 }
